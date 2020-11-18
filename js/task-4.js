d4d03a4058f69be276c96f95397df797be8e3b35
Maksym Humeniuk <maizeproduction@gmail.com>
2020-11-18 18:34:07 +0200

    finishing homework

--- js/task-4.js
+++ js/task-4.js
@@ -3,7 +3,6 @@ const totalValueRef = document.querySelector('#value');
 const incrementRef = document.querySelector('button[data-action="increment"]');
 const decrementRef = document.querySelector('button[data-action="decrement"]');
 
-
 incrementRef.addEventListener('click', () => {
 totalValueRef.textContent = counterValue +=1;
 })
